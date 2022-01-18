48174293800556e0bde1e0a62b10a7bd88a001db
lê dung <ledung.14111998@gmail.com>
2022-01-18 22:50:10 +0700

    first commit

--- SRC/components/header.js
+++ SRC/components/header.js
@@ -7,34 +7,33 @@ const Header = {
             <img src="https://phothongcaodang.fpt.edu.vn/wp-content/uploads/LOGO-PTC%C4%90-White-Transparency.png"
                 alt="" width="150px" class="mx-auto">
         </a>
+        </div>
+        <div class="header-main bg-amber-600 flex ">
+        <nav class="ml-6">
+            <ul class="flex py-3" id="menu" >
+                <li class="">
+                    <a href="/"  class="text-white p-4 hover:text-pink-900">Trang Chủ</a>
+                </li>
+                <li class="">
+                    <a href="/signup" class="text-white p-4 hover:text-pink-900">Đăng ký</a>
+                </li>
+                <li class="">
+                    <a href="/signin" class="text-white p-4 hover:text-pink-900">Đăng nhập</a>
+                </li>
+                <li class="">
+                    <a href="/admin/dashboard" class="text-white p-4 hover:text-pink-900">Admin</a>
+                </li>
+            </ul>
+        </nav>
+        <div class="search flex ml-80 py-3">
+            <input type="text"
+                class=" h-6 pr-4 mr-2   placeholder:italic rounded-none placeholder:text-gray-400 block bg-white w-full border border-gray-300 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm ">
+            <input type="button" value="tìm kiếm" class="border-2 bg-indigo-900 text-white uppercase h-6 px-8 ">
+        </div>
     </div>
 
-    <nav class="flex justify-between p-3 bg-orange-400 items-center ">
-    <div class="menu text-white">
-    <ul class="flex inset-4 font-semibold text-white" id="menu" >
-        <li class="">
-            <a href="/"  class="menu-item__link mr-10 text-Slate-50">Trang Chủ</a>
-        </li>
-        <li class="">
-            <a href="/signup" class=" mr-10 hover:text-pink-900">Tuyển Sinh</a>
-        </li>
-        <li class="">
-            <a href="/signin" class=" mr-10 hover:text-pink-900">Góc Sinh viên</a>
-        </li>
-        <li class="">
-            <a href="/signup" class=" mr-10 hover:text-pink-900">Trương Trình Đào tạo</a>
-        </li>
-        <li class="">
-            <a href="/admin/dashboard" class=" mr-10 hover:text-pink-900">Tuyển Dụng</a>
-        </li>
-       
-    </ul>
-    </div>
-    <div class="search">
-    <input type="text" class="p-0.2" />
-    <button class="bg-blue-500 active:bg-blue-600 ...">
-      Tìm Kiếm
-    </button>
+    
+   
 </div>
 </nav>
         `;
